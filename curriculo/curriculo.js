237f5b1192779e892a6c51c069a1ffa4016e3b5c
Saito132 <yuri.japa1305@gmail.com>
2025-04-08 00:32:48 -0300

    Add files via upload

---
+++ curriculo/curriculo.js
@@ -0,0 +1,17 @@
+const muda = document.getElementById('muda')
+
+muda.addEventListener("mouseover",event=>{
+
+muda.innerHTML = 'Yuri Henzo Saito?'
+
+
+
+})
+
+muda.addEventListener("mouseout",event=>{
+
+muda.innerHTML = 'Yuri Henzo Saito'
+
+
+
+})
